56d885d2a09f8b6301caae6c567ea2e050729b7a
Denis Brevnov <denisbrevnov2006@gmail.com>
2025-05-17 02:29:25 +0500

    add threading

--- socket_server.py
+++ socket_server.py
@@ -1,38 +1,49 @@
 import socket
-import threading
+import threading 
 import os
-  
+
+class ClientThread(threading.Thread):
+
+    def __init__(self, clientAddress, clientSock):
+        threading.Thread.__init__(self)
+        self.csocket = clientSock
+        print('Hello!', clientAddress)
+    
+    def run(self):
+        working_dir = os.getcwd()
+        while True:
+            request = self.csocket.recv(1024).decode().split('\n')
+            print(request)
+
+            method, url, protocol = request[0].split(' ')
+            url = os.path.join(working_dir, url[1:])
+            print(url)
+
+            code_error = "404 Not Found"
+            body = ""
+
+            if os.path.isdir(url):
+                url = os.path.join(url,"index.htm")
+
+            if os.path.isfile(url):
+                code_error = "200 OK"
+                body = open(url, 'r').read()
+                responce = f"HTTP/1.1 {code_error}\n" + "Server:my_server" + "\n\n" + body
+                self.csocket.send(responce.encode())
+                
+            self.csocket.close()
+            print("Connection close, bye!\n")
+
+    
 
 def main():
-    working_dir = os.getcwd()
     server = socket.socket()
-    server.bind(('127.0.0.1', 4545))
+    server.bind(('127.0.0.1', 4646))
     server.listen(1)
-    clientSock, clientAddress = server.accept()
     while True:
-        print('Hello! I am :', clientAddress)
-        request = clientSock.recv(10240).decode().split('\n')
-        print(request)
-
-        method, url, protocol = request[0].split(' ')
-        url = os.path.join(working_dir, url[1:])
-        print(url)
-
-        code_error = "404 Not Found"
-        body = ""
-
-        if os.path.isdir(url):
-            url = os.path.join(url,"index.htm")
-
-        if os.path.isfile(url):
-            code_error = "200 OK"
-            body = open(url, 'r').read()
-            responce = f"HTTP/1.1 {code_error}\n" + "Server:my_server" + "\n\n" + body
-            clientSock.send(responce.encode())
-            clientSock.close()
-
-        print("Connection close, bye!\n")
-
+        clientSock, clientAddress = server.accept()
+        newThread = ClientThread(clientAddress, clientSock)
+        newThread.start()
 
 
 if __name__ == '__main__':
