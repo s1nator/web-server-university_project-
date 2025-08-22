329ff33665fd48493b112bb9f5fd21797e82c52f
Denis Brevnov <denisbrevnov2006@gmail.com>
2025-08-23 02:49:55 +0500

    add async files

--- configuration.py
+++ configuration.py
@@ -1,9 +1,9 @@
 import os
 
 PROXY_TARGET_HOST = "127.0.0.1"
-PROXY_TARGET_PORT = 5000
+PROXY_TARGET_PORT = 80
 host = "127.0.0.1"
-port = 4646
+port = 4747
 working_directory = os.getcwd()
 date_logs_delete = "2026-01-01 00:00:00"
 home_file = "index.htm"
--- web_server.py
+++ web_server.py
@@ -1,18 +1,21 @@
-from configuration import working_directory, host, port, date_logs_delete, PROXY_TARGET_HOST, PROXY_TARGET_PORT
+from configuration import host, port, date_logs_delete, PROXY_TARGET_HOST, PROXY_TARGET_PORT
 import asyncio
+import aiofiles
 import time
 import ssl
-import os
 
 
 async def write_in_file(logs, date_delete):
-    if date_delete == time.localtime(time.time()):
-        with open("access.log", 'r') as f:
+    current_time = time.localtime()
+    current_time_str = time.strftime("%Y-%m-%d %H:%M:%S", current_time)
+    if date_delete == current_time_str:
+        async with aiofiles.open("access.log", 'r') as f:
             pass
     else:
-        with open("/Users/denisbrevnov/Documents/Files/code/Python/Python_FIIT/Python_TASK/web_server_repo/web-server-university_project-/access.log", 'a', encoding="utf-8") as f:
-            f.write(str(logs) + "\n")
-            f.close()
+        async with aiofiles.open("/Users/denisbrevnov/Documents/Files/code/Python/Python_FIIT/Python_TASK/web_server_repo/web-server-university_project-/access.log", 'a', encoding="utf-8") as f:
+            await f.write(str(logs) + "\n")
+            await f.close()
+
 
 async def read_requests(target_reader):
     delimiter = b'\r\n\r\n'
@@ -30,6 +33,7 @@ async def read_requests(target_reader):
 
 async def serve_client(reader, writer):
     try:
+        client_address = writer.get_extra_info('peername')
         request_bytes = await read_requests(reader)
         if request_bytes is None:
             print(f'Client unexpectedly disconnected')
@@ -45,9 +49,11 @@ async def serve_client(reader, writer):
 
         target_response = await read_requests(target_reader)
 
-        target_request = target_response.decode("utf-8")
+        response = target_response.decode("utf-8")
 
-        response = target_request
+        request_for_logs = response.split("\r\n")
+        time_for_logs = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        logs = f"{request_for_logs[1]}|{time_for_logs}|{client_address[0]}|{request_for_logs[0]}"
 
         writer.write(response.encode("utf-8"))
         await writer.drain()
@@ -56,6 +62,7 @@ async def serve_client(reader, writer):
         target_writer.close()
         await writer.wait_closed()
         await target_writer.wait_closed()
+        await write_in_file(logs, date_logs_delete)
     except Exception as exception:
         print(f"Error {exception}")
 
