3bdaa8ac21715add5e873c819f79ecd0d6373908
Denis Brevnov <denisbrevnov2006@gmail.com>
2025-08-22 23:36:11 +0500

    add proxy passs, delete auto-indexing files

--- configuration.py
+++ configuration.py
@@ -1,7 +1,14 @@
 import os
 
+PROXY_TARGET_HOST = "127.0.0.1"
+PROXY_TARGET_PORT = 5000
 host = "127.0.0.1"
 port = 4646
 working_directory = os.getcwd()
 date_logs_delete = "2026-01-01 00:00:00"
 home_file = "index.htm"
+
+virtual_hosts = {
+    "folder.com" : "/Users/denisbrevnov/Documents/Files/code/Python/Python_FIIT/Python_TASK/web_server_repo/web-server-university_project-/folderserver",
+    "default" : "/Users/denisbrevnov/Documents/Files/code/Python/Python_FIIT/Python_TASK/web_server_repo/web-server-university_project-/index.htm"
+}
--- web_server.py
+++ web_server.py
@@ -1,4 +1,4 @@
-from configuration import working_directory, host, port, home_file, date_logs_delete
+from configuration import working_directory, host, port, date_logs_delete, PROXY_TARGET_HOST, PROXY_TARGET_PORT
 import asyncio
 import time
 import ssl
@@ -14,11 +14,11 @@ async def write_in_file(logs, date_delete):
             f.write(str(logs) + "\n")
             f.close()
 
-async def read_requests(reader):
+async def read_requests(target_reader):
     delimiter = b'\r\n\r\n'
     requests = bytearray()
     while True:
-        chunk = await reader.read(4096)
+        chunk = await target_reader.read(4096)
         if not chunk:
             break
 
@@ -29,152 +29,36 @@ async def read_requests(reader):
     return None
 
 async def serve_client(reader, writer):
+    try:
+        request_bytes = await read_requests(reader)
+        if request_bytes is None:
+            print(f'Client unexpectedly disconnected')
+            writer.close()
+            await writer.wait_closed()
+            return
 
-    request_bytes = await read_requests(reader)
-    if request_bytes is None:
-        print(f'Client unexpectedly disconnected')
+        target_reader, target_writer = await asyncio.open_connection(PROXY_TARGET_HOST, PROXY_TARGET_PORT)
+        print(f"Connected to {PROXY_TARGET_HOST}:{PROXY_TARGET_PORT}")
+
+        target_writer.write(request_bytes)
+        await target_writer.drain()
+
+        target_response = await read_requests(target_reader)
+
+        target_request = target_response.decode("utf-8")
+
+        response = target_request
+
+        writer.write(response.encode("utf-8"))
+        await writer.drain()
+        print('Close connection')
         writer.close()
+        target_writer.close()
         await writer.wait_closed()
-        return
+        await target_writer.wait_closed()
+    except Exception as exception:
+        print(f"Error {exception}")
 
-    request = request_bytes.decode("utf-8")
-    response = await handle_request(request)
-
-    writer.write(response.encode("utf-8"))
-    await writer.drain()
-    print('Close connection')
-    writer.close()
-    await writer.wait_closed()
-
-
-async def handle_request(request):
-
-    logs = []
-
-    request_lines = request.splitlines()[0]
-    method, url, protocol = request_lines.split(" ", 2)
-    path = os.path.join(working_directory, url.lstrip("/"))
-
-
-    if url.split("/")[-1] == "":
-        code_error = "200 OK"
-        request_for_logs = request.split("\n")
-        logs.append(
-            [request_for_logs[1].strip("\r"),
-             time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
-             request_for_logs[0].strip("\r"),
-             code_error,
-             request_for_logs[2].strip("\r"),
-             request_for_logs[3].strip("\r")])
-        with open("index.htm", "r", encoding="utf-8") as f:
-            body = f.read()
-        response = f"HTTP/1.1 {code_error}\n" + "Server:my_server" \
-                   + "\n\n" + body
-
-    elif os.path.isdir(path):
-        code_error = "200 OK"
-        request_for_logs = request.split("\n")
-        logs.append([request_for_logs[1].strip("\r"),
-                     time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
-                     request_for_logs[0].strip("\r"),
-                     code_error,
-                     request_for_logs[2].strip("\r"),
-                     request_for_logs[3].strip("\r")])
-
-        list_files = os.listdir(path)
-        name_folder = url.split("/")[-1]
-        body = f"<html> \
-                        <head></head>\
-                        <body>\
-                            <h1>{name_folder}</h1>\
-                            <hr>\
-                        "
-
-        for file in list_files:
-            if os.path.isfile(path):
-                body += f"<h4><a href={path}>{file}</a></h4>"
-            if os.path.isdir(path):
-                working_dir = path
-                body += f"<h4><a href={working_dir}>{file}</a></h4>"
-        body += f"<hr>"
-
-        response = f"HTTP/1.1 {code_error}\n" + "Server:my_server" \
-                   + "\n\n" + body
-
-    elif os.path.isfile(path):
-        code_error = "200 OK"
-        request_for_logs = request.split("\n")
-        logs.append(
-            [request_for_logs[1].strip("\r"),
-             time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
-             request_for_logs[0].strip("\r"),
-             code_error,
-             request_for_logs[2].strip("\r"),
-             request_for_logs[3].strip("\r")])
-        body = open(url.split("/")[-1], "rb").read().decode("utf-8")
-        response = f"HTTP/1.1 {code_error}\n" + "Server:my_server" \
-            + "\n\n" + body
-
-    elif 'indexof' in url.split("/")[-1]:
-        code_error = "200 OK"
-        request_for_logs = request.split("\n")
-
-        logs.append([request_for_logs[1].strip("\r"),
-                      time.strftime( "%Y-%m-%d %H:%M:%S", time.localtime()),
-                     request_for_logs[0].strip("\r"),
-                     code_error,
-                     request_for_logs[2].strip("\r"),
-                     request_for_logs[3].strip("\r")])
-
-        list_files = os.listdir(working_directory)
-        index_of_name = "Index of /"
-
-        body = f"<html> \
-                <head></head>\
-                <body>\
-                    <h1>{index_of_name}</h1>\
-                    <hr>\
-                "
-
-        for file in list_files:
-            if os.path.isfile(file):
-                body += f"<h4><a href={file}>{file}</a></h4>"
-            if os.path.isdir(file):
-                working_dir = os.path.join(working_directory, file)
-                body += f"<h4><a href={working_dir}>{file}</a></h4>"
-        body += f"<hr>"
-
-        response = f"HTTP/1.1 {code_error}\n" + "Server:my_server" \
-            + "\n\n" + body
-
-    else:
-        code_error = "404 Not Found"
-        request_for_logs = request.split("\n")
-        logs.append(
-            [request_for_logs[1].strip("\r"),
-             time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
-             request_for_logs[0].strip("\r"),
-             code_error,
-             request_for_logs[2].strip("\r"),
-             request_for_logs[3].strip("\r")])
-
-        body = f"<html> \
-                <head></head>\
-                <body>\
-                    <center>\
-                        <h1>404 Not Found</h1>\
-                    </center>\
-                    <hr>\
-                    <center>Denis server/ 1.0.0</center>\
-                </body>\
-            </html>"
-        response = f"HTTP/1.1 {code_error}\n" + "Server:my_server" + \
-              "\n\n" + body
-
-    await write_in_file(logs, date_logs_delete)
-    return response
-    
-    
 
 async def main():
     sslcontext = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
